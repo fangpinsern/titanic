58df33bd94ec2d734435aed1bea59a9294cc2fb9
pinsern <pinsern@PS-Mac.com>
2021-08-01 02:21:31 +0800

    Added rejection function

--- src/components/bookingRequestApproveReject.js
+++ src/components/bookingRequestApproveReject.js
@@ -29,12 +29,43 @@ const BookingRequestApproveRejectComponent = (props) => {
     });
     // console.log(responseData);
   };
+
+  const rejectFunction = async () => {
+    const response = await fetch(
+      process.env.REACT_APP_BACKEND_URL + "/api/v1/bookingreq/reject",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          bookingRequestId: props.bookingRequestId,
+        }),
+        headers: {
+          Authorization: process.env.REACT_APP_BACKEND_AUTH,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      Modal.error({
+        title: "This is an error message",
+        content: responseData.message,
+      });
+    }
+
+    Modal.success({
+      content: "Rejected",
+    });
+    // console.log(responseData);
+  };
   return (
     <Space>
       <Button type="primary" onClick={approveFunction}>
         Approve
       </Button>
-      <Button type="danger">Reject</Button>
+      <Button type="danger" onClick={rejectFunction}>
+        Reject
+      </Button>
     </Space>
   );
 };
